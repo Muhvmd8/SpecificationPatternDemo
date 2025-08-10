57c8ab076fd9de63d1bb141ca65f76725db88555
Muhvmd8 <ma7007167@gmail.com>
2025-08-10 17:52:10 +0300

    Product Model

--- SpecificationPatternDemo/Models/Product.cs
+++ SpecificationPatternDemo/Models/Product.cs
@@ -1,7 +1,19 @@
 ﻿namespace SpecificationPatternDemo.Models;
 internal class Product 
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = default!;
+    public string Name { get; set; }
+    public string Category { get; set; }
     public decimal Price { get; set; }
+
+    public Product(string name, string category, decimal price)
+    {
+        Name = name;
+        Category = category;
+        Price = price;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} - {Category} - {Price:C}";
+    }
 }
